404f21bfdb16e044c80da04f6d8e01f8c2cc4973
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-08-21 16:39:27 +0330

    feat(header): add search input box

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
+
 export default function page() {
   return (
-    <div className='flex-row-center min-h-screen text-5xl'>
-        <h1>Home page</h1>
-    </div>
+    <div className='flex-row-center font-semibold text-6xl mt-24'> body </div>
   )
 }
--- src/components/shared/header/Header.tsx
+++ src/components/shared/header/Header.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import Image from 'next/image'
+import SearchIcon from '@/components/icons/SearchIcon'    
+import LenzIcon from '@/components/icons/LenzIcon'
 import './header.css'
-    
 export default function Header() {
   return (
-    <header className=''>
-      w
+    <header className='bg-white shadow p-4'>
+      <div className='header__wrapper container'>
+        <div className='flex items-center gap-7'>
+          <div>
+            <Image src="./images/logo.svg" alt="Description" width={133} height={133} />
+          </div>
+
+          <div className='bg-amber-500 w-full'>
+            <form action="">
+              <div className='flex items-center justify-between gap-x-2 border-b border-b-black p-2 bg-[var(--mainGray)]'>
+                <div className='flex items-center gap-x-2'>
+                  <SearchIcon />
+                  <input type="text" placeholder='Artist, category, topic...' className=' outline-none'/>
+                </div>
+                <LenzIcon />
+              </div>
+            </form>
+          </div>
+
+          <div>
+
+          </div>
+        </div>
+
+        <div></div>
+      </div>
     </header>
   )
 }
