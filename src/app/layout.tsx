6f0d0f983fa5d2b3330690bae0e31e3503cd63aa
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-08-29 14:16:29 +0330

    add svg attribute for manage icons simply

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,11 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <svg className="hidden">
+        <symbol id="leftArrow" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.7} stroke="currentColor" className="w-7 h-7">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
+        </symbol>
+      </svg>
       <body>{children}</body>
     </html>
   )
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,8 +6,8 @@ export default function NotFoundPage() {
       <div className='flex-col-center gap-7'>
         <Image src="/images/404.svg" alt="Not Found" width={777} height={777} />
         <a href="./" className='border rounded-full px-6 py-1.5 text-white flex-row-center gap-x-2 hover:bg-white hover:text-[#381E71]'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.7} stroke="currentColor" className="w-7 h-7">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
+          <svg className="w-6 h-6">
+            <use href="#leftArrow" />
           </svg>
           Back To Home
         </a>
