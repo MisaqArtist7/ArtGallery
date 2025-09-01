74f5e9ddb2b31da7aeca8ff3f9f562677161e023
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-09-01 15:11:26 +0330

    feat(homepage): add exhibition section

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,35 @@
+'use client'
 import React from 'react'
 import Carousel from '@/components/shared/Carousel/Carousel'
+import editorals from '@/data/Editorials'
 import Link from 'next/link'
 import Image from 'next/image'
-export default function page() {
+export default function HomePage() {
+  const [blogs] = React.useState(editorals)
   return (
     <>
       <section className='heroSection'> 
         <Carousel />
       </section>
+
+      <section className='bg-[#222222] container w-full flex justify-center items-center mx-auto py-7'>
+        <div className='grid grid-cols-12 gap-11 items-center w-full'>
+
+          <div className='col-span-6 flex flex-col justify-center items-start gap-5 w-[70%] ml-11 text-white'>
+            <h2 className='uppercase text-4xl'>THE SOUND OF FORM: REPRISE</h2>
+            <p className='text-sm'>
+              Featuring works by Kevin Jackson and Andrew Crane, Reprise explores the boundless ways in which form, rhythm, silence, and resonance coexist on canvas.
+            </p>
+            <Link href="" className='fourthBtn'>view exhibition</Link>
+          </div>
+
+          <div className="col-span-6 relative w-full h-[400px]">
+            <Image src="/images/Exhibition.webp" alt="Personality" fill style={{ objectFit: 'cover' }} />
+          </div>
+
+        </div>
+      </section>
+
       <section className='bg-[var(--mainGray)]'>
         <div className='container py-16 w-full flex justify-center items-center mx-auto'>
           <div>
@@ -15,84 +37,30 @@ export default function page() {
             <div>
 
               <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5'>
-                <div className='shadow bg-white flex flex-col justify-between'>
-                  <Image src="/images/Editorial/1.jpg" alt="Personality" width={369} height={369} />
-                  <div className='p-4 flex flex-col justify-between gap-7 h-full'>
+              {blogs.map((item, index) => (
+                <div key={index} className='shadow bg-white flex flex-col justify-between'>
+                    <Image src={item.src} alt="Personality" width={369} height={369} />
+                    <div className='p-4 flex flex-col justify-between gap-7 h-full'>
 
-                    <div className='flex flex-col justify-center gap-1'>
-                      <span className='text-gray-500 uppercase text-sm'>Artists</span>
-                      <h3 className='text-lg'>Tenderbeing: Ariadna Dane</h3>
-                    </div>
-                    
-                    <div className='flex items-center justify-between'>
-                      <span className='text-gray-500 uppercase text-sm'>By Sophie Heatley | 06 Aug 2025</span>
-                      <a href="">
-                        <svg className='w-5 h-5'>
-                          <use href="#share" />
-                        </svg>
-                      </a>
-                    </div>
+                      <div className='flex flex-col justify-center gap-1'>
+                        <span className='text-gray-500 uppercase text-sm'>{item.field}</span>
+                        <h3 className='text-lg'>{item.title}</h3>
+                      </div>
+                      
+                      <div className='flex items-center justify-between'>
+                        <span className='text-gray-500 uppercase text-sm'>{item.date}</span>
+                        <a href="">
+                          <svg className='w-5 h-5'>
+                            <use href="#share" />
+                          </svg>
+                        </a>
+                      </div>
 
-                  </div>
+                    </div>
                 </div>
-
-                <div className='shadow bg-white flex flex-col justify-between'>
-                  <Image src="/images/Editorial/2.jpg" alt="Personality" width={369} height={369} />
-                  <div className='p-4 flex flex-col justify-between gap-7 h-full'>
-
-                    <div className='flex flex-col justify-center gap-1'>
-                      <span className='text-gray-500 uppercase text-sm'>Artists</span>
-                      <h3 className='text-lg'>Valentin Renner on String Art: &quot;I&apos;m exploring order, chaos, and the spaces in between.&quot;</h3>
-                    </div>
-                    
-                    <div className='flex items-center justify-between'>
-                      <span className='text-gray-500 uppercase text-sm'>By Sophie Heatley | 06 Aug 2025</span>
-                      <svg className='w-5 h-5'>
-                        <use href="#share" />
-                      </svg>
-                    </div>
-
-                  </div>
-                </div>
-
-                <div className='shadow bg-white flex flex-col justify-between'>
-                  <Image src="/images/Editorial/3.jpg" alt="Personality" width={369} height={369} />
-                  <div className='p-4 flex flex-col justify-between gap-7 h-full'>
-
-                    <div className='flex flex-col justify-center gap-1'>
-                      <span className='text-gray-500 uppercase text-sm'>Artists</span>
-                      <h3 className='text-lg'>Tenderbeing: Ariadna Dane</h3>
-                    </div>
-                    
-                    <div className='flex items-center justify-between'>
-                      <span className='text-gray-500 uppercase text-sm'>By Sophie Heatley | 06 Aug 2025</span>
-                      <svg className='w-5 h-5'>
-                        <use href="#share" />
-                      </svg>
-                    </div>
-
-                  </div>
-                </div>
-
-                <div className='shadow bg-white flex flex-col justify-between'>
-                  <Image src="/images/Editorial/4.jpg" alt="Personality" width={369} height={369} />
-                  <div className='p-4 flex flex-col justify-between gap-7 h-full'>
-
-                    <div className='flex flex-col justify-center gap-1'>
-                      <span className='text-gray-500 uppercase text-sm'>Artists</span>
-                      <h3 className='text-lg'>Tenderbeing: Ariadna Dane</h3>
-                    </div>
-                    
-                    <div className='flex items-center justify-between'>
-                      <span className='text-gray-500 uppercase text-sm'>By Sophie Heatley | 06 Aug 2025</span>
-                      <svg className='w-5 h-5'>
-                        <use href="#share" />
-                      </svg>
-                    </div>
-
-                  </div>
-                </div>
+              ))}
               </div>
+
               <div className='py-6 flex-row-center'>
                 <a href="" className='uppercase btnSecoundary'>Show All</a>
               </div>
@@ -110,7 +78,7 @@ export default function page() {
 
       <section className='bg-white container w-full flex justify-center items-center mx-auto'>
         <div className='grid grid-cols-12 gap-11 items-center w-full'>
-          
+
           <div className='col-span-6 flex flex-col justify-center items-start gap-5 w-[70%] ml-11'>
             <h2 className='uppercase text-4xl'>What&apos;s your Art Personality?</h2>
             <p className='text-gray-500 text-sm'>
---
+++ src/data/Editorials.tsx
@@ -0,0 +1,7 @@
+const editorals = [
+    {id:1, src:"/images/Editorial/1.jpg", field:'Teacher',  title: "Tenderbeing: Ariadna Dane", date: 'By Sophie Heatley | 06 Aug 2025'},
+    {id:2, src:"/images/Editorial/2.jpg", field:'Artist',   title: "Valentin Renner on String Art: \"I'm exploring order, chaos, and the spaces in between.\"  ", date: 'By Rise Art | 02 Jul 2025'},
+    {id:3, src:"/images/Editorial/3.jpg", field:'musician', title: "Tuyo: \"Somewhere between the sacred and the profane.\"", date: 'By Sophie Heatley | 13 Jun 2025'},
+    {id:4, src:"/images/Editorial/4.jpg", field:'Painter',  title: "Jasmine Mansbridge: Postcards from Another Realm", date: 'By Sophie Heatley | 29 Apr 2025'},
+]
+export default editorals;
