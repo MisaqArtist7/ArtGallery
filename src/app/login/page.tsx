89e2e554a62ab911df0babbb486f885befc3c976
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-09-23 14:08:56 +0330

    fix bug

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,13 +9,13 @@ export default function LoginPage() {
                 <div className='flex justify-between items-center'>   
                     <h1 className='font-semibold text-xl'>Sign up</h1>
                     <span className='hover:cursor-pointer'>
-                        <svg className='w-6 h-6 text-gray-600'>
+                        <svg className='w-6 h-6 hover:text-[var(--primaryBlue)]'>
                             <use href="#backspace" />
                         </svg>
                     </span>
                 </div>
 
-                <Link href="#" className='flex justify-center items-center gap-2 border-2 border-gray-300 px-7 py-2 rounded-md'>
+                <Link href="#" className='flex justify-center items-center gap-2 border-2 border-gray-300 hover:border-[var(--primaryBlue)]/40 px-7 py-2 rounded-md'>
                     <Image src="/images/google.svg" alt="Description" width={17} height={17} />
                     Log in with Google
                 </Link>
@@ -23,10 +23,6 @@ export default function LoginPage() {
                 <span className='border-1 border-gray-200 mb-3 my-2 w-[calc(95%-1rem)] mx-auto'></span>
 
                 <form action="" className='flex flex-col gap-3'>
-                    <div className='flex flex-col gap-2'>
-                        <label htmlFor="">Name</label>
-                        <input type="text" className='flex justify-center items-center gap-2 border-2 border-gray-300 focus:outline-[var(--primaryBlue)] px-3 py-2 rounded-md' placeholder='Enter your name'/>
-                    </div>
 
                     <div className='flex flex-col gap-2'>
                         <label htmlFor="">Email</label>
@@ -40,7 +36,7 @@ export default function LoginPage() {
 
                     <div className='flex items-center gap-1 my-3'>
                         <input type="checkbox" id="terms" />
-                        <label className='text-gray-600'>I agree with <span>Terms</span> and <span>Privacy</span></label>
+                        <label className='text-gray-600'> Remember me</label>
                     </div>
 
                     <button className='authBtn'>Sign up</button>
@@ -49,8 +45,8 @@ export default function LoginPage() {
                 <span className='border-1 border-gray-200 mb-3 my-2 w-[calc(95%-1rem)] mx-auto'></span>
 
                 <div className='flex-col-center text-gray-700'>
-                    <span className='text-md'>Already have an account?</span>
-                    <Link href="/" className='text-[var(--primaryBlue)]'>Log in</Link>
+                    <span className='text-md'>haven&apos;t an account yet?</span>
+                    <Link href="/register" className='text-[var(--primaryBlue)] hover:underline'>Sign up</Link>
                 </div>
             </div>
         </section>
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -10,15 +10,15 @@ export default function RegisterPage() {
                 <div className='flex justify-between items-center'>   
                     <h1 className='font-semibold text-xl'>Sign up</h1>
                     <span className='hover:cursor-pointer'>
-                        <svg className='w-6 h-6 text-gray-600'>
+                        <svg className='w-6 h-6 hover:text-[var(--primaryBlue)]'>
                             <use href="#backspace" />
                         </svg>
                     </span>
                 </div>
 
-                <Link href="#" className='flex justify-center items-center gap-2 border-2 border-gray-300 px-7 py-2 rounded-md'>
+                <Link href="#" className='flex justify-center items-center gap-2 border-2 border-gray-300 hover:border-[var(--primaryBlue)]/40 px-7 py-2 rounded-md'>
                     <Image src="/images/google.svg" alt="Description" width={17} height={17} />
-                    Log in with Google
+                    Sign up with Google
                 </Link>
 
                 <span className='border-1 border-gray-200 mb-3 my-2 w-[calc(95%-1rem)] mx-auto'></span>
@@ -41,7 +41,7 @@ export default function RegisterPage() {
 
                     <div className='flex items-center gap-1 my-3'>
                         <input type="checkbox" id="terms" />
-                        <label className='text-gray-600'>I agree with <span>Terms</span> and <span>Privacy</span></label>
+                        <label className='text-gray-600'>I agree with <span className='text-[var(--primaryBlue)]'>Terms</span> and <span className='text-[var(--primaryBlue)]'>Privacy</span></label>
                     </div>
 
                     <button className='authBtn'>Sign up</button>
@@ -51,7 +51,7 @@ export default function RegisterPage() {
 
                 <div className='flex-col-center text-gray-700'>
                     <span className='text-md'>Already have an account?</span>
-                    <Link href="/login  " className='text-[var(--primaryBlue)]'>Log in</Link>
+                    <Link href="/login  " className='text-[var(--primaryBlue)] hover:underline'>Log in</Link>
                 </div>
             </div>
         </section>
