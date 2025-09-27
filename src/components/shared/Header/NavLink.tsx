e4fbf239664c853bcff12fa70609995d97658bb6
MisaqArtist7 <misaqbabakhani777@gmail.com>
2025-09-27 14:22:15 +0330

    fix header

---
+++ src/components/shared/Header/NavLink.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React from 'react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation';
+type NavLinksProps = {
+  href: string;
+  children: React.ReactNode;
+};
+
+export default function NavLink({href, children}: NavLinksProps) {
+  const path = usePathname();
+  return (
+    <Link href={href} className={path === href ? "active" : "link"}>
+        {children}
+    </Link>
+  )
+}
